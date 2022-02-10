4acd120c1d03e3b62315039e56cb7f8c84640384
hayaa1234 <hayalawansah@gmail.com>
2022-02-10 19:39:03 +0200

    adding bigger be mouse hover feature

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
 let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 let c = canvas.getContext("2d");
-// c.fillStyle="#ff4562"
-// c.fillRect(100,100,50,50)
-// c.fillRect(50,50,50,50)
-// c.fillRect(0,0,50,50)
-// c.fillRect(0,100,50,50)
-// c.fillRect(100,0,50,50)
-// c.fillRect(150,50,50,50)
 
-// c.beginPath();
-// c.moveTo(500,500)
-// c.lineTo(700,700)
-// c.lineTo(200,700)
-// c.strokeStyle = "green"
-// c.stroke()
+let color_arr = ["#845EC2","#D65DB1","#FF6F91","#FF9671","#FFC75F","#F9F871"]
 
-// c.beginPath()
-// c.arc(300,300,100,0,Math.PI*2,false)
-// c.stroke()
+let mouse = {
+    x:undefined,
+    y:undefined
+}
+let max_r = 40
+let min_r =2
+window.addEventListener("mousemove",(e)=>{
+    mouse.x = e.x
+    mouse.y = e.y
+})
+window.addEventListener("resize",()=>{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init()
+})
 
-// let col = 6
-// let hexa = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
-// for(let i =0 ; i<100;i++){
-//     c.beginPath()
-//     let x=Math.random()*window.innerWidth
-//     let y=Math.random()*window.innerHeight
-//     c.arc(x,y,50,0,Math.PI*2,false)
-//     c.strokeStyle = `#${hexa[Math.floor(Math.random()*16)]}${hexa[Math.floor(Math.random()*16)]}${hexa[Math.floor(Math.random()*16)]}${hexa[Math.floor(Math.random()*16)]}${hexa[Math.floor(Math.random()*16)]}${hexa[Math.floor(Math.random()*16)]}`
-//     c.stroke()
-// }
-function Circle(x,y,dx,dy,r){
+function Circle(x,y,dx,dy,r,color){
     this.x=x
     this.y =y 
     this.dx = dx
     this.dy =dy
     this.r = r
-    this.all = this
+    this.color =  color
+    this.min_radius =r
     this.draw = function(){
         c.beginPath();
         c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-        c.strokeStyle = "red";
+        c.strokeStyle = this.color;
         c.stroke();
-        // c.fillStyle = "red";
-        // c.fill();
+        c.fillStyle = this.color;
+        c.fill();
     }
     this.update = function(){
         if (this.x+this.r>innerWidth||this.x-this.r<0){
             this.dx =-this.dx
          }
-         if (this.y+this.r>innerHeight || this.y-this.r<0){
+        if (this.y+this.r>innerHeight || this.y-this.r<0){
              this.dy=-this.dy
          }
-         this.x+=this.dx
+
+        this.x+=this.dx
         this.y+=this.dy
+        if(mouse.x - this.x <50 && mouse.x -this.x > -50 && mouse.y - this.y <50 && mouse.y -this.y > -50 ){
+            if (this.r <max_r){
+                this.r +=2 
+            }
+        } // this condition means that any circle in the area of raduis 50 will increase in its raduis 
+        else if (this.r > this.min_radius){
+            this.r -=1
+        }
         this.draw()
-
     }
-
 }
+
 let circle_arr = []
-for (let i =0 ; i<100 ; i++){
-    let x=Math.random()*innerWidth
-    let y= Math.random()*innerHeight
-    let dx = (Math.random()-0.5)
-    let dy = (Math.random()-0.5)
-    let circle = new Circle(x,y,dx,dy,30)
-    circle_arr.push(circle)
+let init = ()=>{
+    circle_arr=[]
+    for (let i =0 ; i<800 ; i++){
+        let x=Math.random()*innerWidth
+        let y= Math.random()*innerHeight
+        let dx = (Math.random()-0.5)
+        let dy = (Math.random()-0.5)
+        let color = color_arr[Math.floor(Math.random()*color_arr.length)]
+        let raduis = Math.random()*3+1
+        let circle = new Circle(x,y,dx,dy,raduis,color)
+        circle_arr.push(circle)
+    }// generate a number of circle objects and add it into one arr
+    
 }
-
-// let circle1 = new Circle(300,200,6,3,50)
-// let circle2 = new Circle(200,200,4,2,10)
+init()
 
 function animate() {
   requestAnimationFrame(animate);
@@ -82,9 +84,6 @@ function animate() {
     circle.update()
   }
   )
-//   circle1.update()
-
-//   circle2.update()
 }
 animate();
 
